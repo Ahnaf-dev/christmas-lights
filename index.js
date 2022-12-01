eb8a1052fd994d5c9385d09c4eeb076b4751df0e
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-01 13:34:55 -0700

    manually activate lights functionality

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const toggleNightBtn = $(".toggle-night");
+const toggleLightsBtn = $(".toggle-lights");
+const lights = $(".lights");
 const body = $("body");
 
 toggleNightBtn.on("click", toggleDark);
+toggleLightsBtn.on("click", toggleLights);
+
+function toggleLights() {
+  lights.toggleClass("active");
+
+  if (lights.hasClass("active")) {
+    toggleLightsBtn.text("Turn Off Lights");
+  } else {
+    toggleLightsBtn.text("Turn On Lights");
+  }
+}
+
+// dark mode functionality
 
 function toggleDark() {
   body.toggleClass("dark");
